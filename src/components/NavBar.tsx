3230025f7a5cd65c90983269256b473d5512f567
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-03-14 16:16:00 +0100

    dark-light mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/iconnguyencoder.png'
+import ColorSwitch from './ColorSwitch'
 
 export const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='2rem'>
         <Image src={logo} boxSize='3rem'/>
-        <Text>NavBar</Text>
+        <ColorSwitch/>
     </HStack>
   )
 }
