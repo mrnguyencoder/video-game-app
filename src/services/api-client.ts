175e8b90ac278382faeff1e6b5fc3fdf005566d3
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-03-17 12:28:06 +0100

    ShiftCommandP: format document

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '86e8b73f2c8648c980c24e7e9fb80ebc'
-   } 
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "86e8b73f2c8648c980c24e7e9fb80ebc",
+  },
+});
